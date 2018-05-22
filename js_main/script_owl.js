6a114f33ccf0596cbf1937ca4e4f9ab10d279bb4
Yana903 <gumenyuk903@mail.com>
2018-05-22 21:23:25 +0300

    with part of Galery, with Carousel, without media

--- js_main/script_owl.js
+++ js_main/script_owl.js
@@ -7,16 +7,17 @@ $('.owl-carousel').owlCarousel({
     singleItem: true,
     navigation: true,
     navigationText: [ 'prev', 'next' ],
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
-        }
-    }
+    items:1
+    // responsive:{
+    //     0:{
+    //         items:1
+    //     },
+    //     600:{
+    //         items:2
+    //     },
+    //     1000:{
+    //         items:3
+    //     }
+    // }
 });
 
